1c68fe107f1906279162029bf56a17443566ac6e
ldhpop123 <dab060719@gmail.com>
2024-07-26 11:37:34 +0900

    20240726

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const riot_API = 'RGAPI-2b3aaa53-82c7-4995-9c4d-9f30dd57d041';
+const riot_API = 'RGAPI-67808b18-3702-4cbc-b8ef-b903236c22b3';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -136,9 +136,60 @@ document.addEventListener('DOMContentLoaded', () => {
     async function match_ids_api(main_server, player_puuid) {
         const response = await axios.get(`https://${main_server}.api.riotgames.com/lol/match/v5/matches/by-puuid/${player_puuid}/ids?start=0&count=20&api_key=${riot_API}`)
         console.log(response.data)
+        const match_ids = response.data
 
-        return response.data
+        return match_ids
     }
+
+    // match_info_api
+    async function match_info_api(main_server, match_ids) {
+        for (let i = 0; i < 20; i++) {
+            const response = await axios.get(`https://${main_server}.api.riotgames.com/lol/match/v5/matches/${match_ids[i]}?api_key=${riot_API}`)
+            console.log(`game_${i}`, response.data)
+
+            const game_index = i
+
+            // player_10
+            player_10(match_ids[i], response.data.participants)
+
+            // game_mode
+            if(response.data.info.queueId == 490) { //일반게임
+                const game_mode = "빠른 대전"
+            }
+            else if (response.data.info.queueId == 450) { // 칼바람
+                const game_mode = "ARAM"
+            }
+            else if (response.data.info.queueId == 440) { // 자랭
+                const game_mode = "자유 랭크"
+            }
+            else if (response.data.info.queueId == 420) { // 솔랭
+                const game_mode = "솔로 랭크"
+            }
+            else if (response.data.info.queueId == 1830 ) { // 집중포화
+                const game_mode = "집중포화"
+            }
+
+            // player_10
+
+
+            display_game_info(
+                game_result, game_mode, game_index,)
+        }
+    }
+
+    function player_10(match_id, player_10) {
+        
+
+        for(let i = 0; i<(player_10.length); i++ ) {
+            const player_10 = {} 
+            const Blue = {}
+            const Red = {}
+            
+
+        }
+    }
+
+
 
     async function display_player_info() {
         // 입력바에서 name#tag를 받아와 #을 기준으로 분리
@@ -164,8 +215,81 @@ document.addEventListener('DOMContentLoaded', () => {
         const rank_li = await rank_api(server, player_summoner_id);
 
         // match_ids_api
-        const result = await match_ids_api(main_server, player_puuid);
-    }   
+        const match_ids = await match_ids_api(main_server, player_puuid);
+
+        // match_info
+        match_info_api(main_server, match_ids)
+    }
+
+    function display_game_info() {
+        document.querySelector('.games').innerHTML += `
+        <div class="game ${game_result}_game" id="game_0">
+            <div class="game_info">
+                <p id="game_mode">${game_mode}</p>
+                <p id="game_date">12일 전</p>
+                <div class="dlvider"></div>
+                <p id="game_result">승리</p>
+                <p id="game_time">26분 31초</p>
+            </div>
+
+            <div class="champion_info">
+                <div class="top">
+                    <div class="champion_icon_level_box">
+                        <div class="champion_icon_box">
+                            <img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/champion/Teemo.png" alt="" id="champion_icon">
+                        </div>
+                        <p id="champion_level">18</p>
+                    </div>
+
+                    <div class="spell_info">
+                        <img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/spell/SummonerFlash.png" alt="" id="spell_f" class="spell">
+                        <img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/spell/SummonerDot.png" alt="" id="spell_d" class="spell">
+                    </div>
+
+                    <div class="rune_info">
+                        <div class="rune_background"><img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/perk/8439.png" alt="" id="main_rune" class="rune"></div>
+                        <div class="rune_background"><img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/perkStyle/8000.png" alt="" id="sub_rune" class="rune"></div>
+                    </div>
+
+                    <div class="kda_info">
+                        <p class="kda"><span id="kill_score">10</span> / <span id="death_score">1</span> / <span id="assist_score">0</span></p>
+                        <p class="kda_score"><span id="kda_score">10.0</span> KDA</p>
+                    </div>
+                </div>
+
+                <div class="bottom">
+                    <div class="items_info">
+                        <div class="item" id="item_0">
+                            <img src="https://opgg-static.akamaized.net/meta/images/lol/14.14.1/item/3031.png" alt="" >
+                        </div>
+                        <div class="item">
+                            <img src="" id="itme_1" onerror="this.style.display='none'">
+                        </div>
+                        <div class="item">
+                            <img src="" id="itme_2" onerror="this.style.display='none'">
+                        </div>
+                        <div class="item">
+                            <img src="" id="itme_3" onerror="this.style.display='none'">
+                        </div>
+                        <div class="item">
+                            <img src="" id="itme_4" onerror="this.style.display='none'">
+                        </div>
+                        <div class="item">
+                            <img src="" id="itme_5" onerror="this.style.display='none'">
+                        </div>
+                        <div class="item">
+                            <img src="" id="ward" onerror="this.style.display='none'">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="game_info">
+
+            </div>
+        </div>`
+        
+    }
 
     // -search_bar-
     const player_name_tag = document.querySelector('#game_name');
@@ -191,7 +315,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // location_select_btn
     select_hidden_box.addEventListener('click', (e) => {
         server_location.textContent = e.target.textContent
-    })
+    });
 
     // search_btn
     search_btn.addEventListener('click', () => {
@@ -200,6 +324,16 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             console.log('입력해주세요');
         }
-    })
+    });
 
+    // game_mode_select_btn
+    document.querySelector('.game_mode_select_box').addEventListener('click', (e) => {
+        console.log(e.target.classList)
+
+        if (e.target.classList[1] == undefined && e.target.classList[0] == 'game_mode_select') {
+            document.querySelector('.selected').classList.remove('selected')
+            e.target.classList.add('selected')
+            // 함수와 연결
+        }
+    })
 });
